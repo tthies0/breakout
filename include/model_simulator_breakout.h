d6cd0ef418e01fc51df6295c360de4d59312d193
Torben Thies <tthies@uni-muenster.de>
2023-10-02 16:28:25 +0200

    Added 3 Tests

--- include/model_simulator_breakout.h
+++ include/model_simulator_breakout.h
@@ -26,33 +26,37 @@ public:
     Paddle getPaddle();
     int getScore();
     GameState getState();
-
-private:
-    GameState _state;
-    int _score = 0;
-
+    
     struct Collision{ // A calculated Collision with a surface normal
         Collision(double x, double y, double surfaceNormalX, double surfaceNormalY, double distance, Collidable* collidedObject)
             : x(x), y(y), surfaceNormalX(surfaceNormalX), surfaceNormalY(surfaceNormalY), distance(distance), collidedObject(collidedObject){};
         double x;
         double y;
-        double surfaceNormalX; //X Part (always normalized)
-        double surfaceNormalY; //Y Part (always normalized)
+        double surfaceNormalX; //X Part (may be normalized)
+        double surfaceNormalY; //Y Part (may be normalized)
         double distance;
         Collidable* collidedObject;
     };
+    
+    bool intersect(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy);//Checks if two line segments intersect
+    Collision getIntersection(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy, Collidable* collidedObject);// gets the intersection point of two lines
+    
+    void reflectBall(Ball& ball, Collision collision); //Reflects the ball given a Collision
+    
+    
+private:
+    GameState _state;
+    int _score = 0;
+
     std::vector<Brick> _bricks;
     std::vector<Ball> _balls;
     Paddle* _paddle;
 
     void checkBorder(Ball& ball); //Checks for Collision with border and changes balls direction
     bool checkHandlePaddleCollision(Ball& ball, Paddle paddle); //Checks for and handle collision with paddle for every ball
-    void reflectBall(Ball& ball, Collision collision); //Reflects the ball given a Collision
     Collision checkCollisionChangeState(Ball& ball); //Checks all intersections and returns the nearest one, notifies Collidable of collision
     Collision calcIntersect(Ball& ball, Collidable& collidable); //Calculates the Intersection of Ball direction and a single Collidable
-    bool intersect(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy);//Checks if two line segments intersect
     bool ccw(double Ax, double Ay, double Bx, double By, double Cx, double Cy);
-    Collision getIntersection(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy, Collidable* collidedObject);// gets the intersection point of two lines
 };
 
 #endif // end of header file
